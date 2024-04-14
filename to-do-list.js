94fcd2df349f998a8a3f977a7b9f546e23fa1216
jyoshithaa_srinivasan <jyoshithaasrinivasan@gmail.com>
2024-04-14 17:54:56 +0530

    added date input and modified storage functions

--- to-do-list.js
+++ to-do-list.js
@@ -1,22 +1,56 @@
-const todoList=[];
+const todoList=[
+
+    {name:'make dinner',
+     dueDate:'2022-12-22'
+},
+{
+    name:'wash dishes',
+    dueDate:'2022-11-04'
+}
+];
 renderTodoList();
 
 function adTodo(){
-    const inputElement=document.querySelector('.js-name-input');
-    const name=inputElement.value;
-    todoList.push(name);
+    const inputNameElement=document.querySelector('.js-name-input');//class selector is parameter
+    const inputDateElement=document.querySelector('.js-date-input');
+    
+    const name=inputNameElement.value;
+    const dueDate=inputDateElement.value;
+    const inputObject={
+        name:name,
+        dueDate:dueDate
+        //shorthand property
+        //name,
+        //dueDate
+    
+    }
+    todoList.push(inputObject);
     console.log(todoList);
-    inputElement.value='';
+    inputNameElement.value='';
+    inputDateElement.value='';
     //resets the text in the textbox on clicking add button
 
     renderTodoList();
    
 }
-function renderTodoList(){
+function renderTodoList(){ //displaying
     let todoListHTML='';
     for(let i=0;i<todoList.length;i++){
-        const todo=todoList[i];
-        const html=`<p>${todo}<button>DELETE</button></p>`; //GENERATING HTML through javascript loop
+        const todoObject=todoList[i];
+        const name=todoObject.name;
+        //SHORTCUT
+        // const {name}=todoObject;
+        //const {name,dueDate}=todo
+        const dueDate=todoObject.dueDate;
+        //template strings as multiline strings
+        const html=`                 
+        <p>
+            ${name} ${dueDate}
+            <button onclick="
+                todoList.splice(${i},1);
+                renderTodoList();         
+            ">DELETE</button>
+        </p>`; //GENERATING HTML through javascript loop
         todoListHTML+=html;
         
 
