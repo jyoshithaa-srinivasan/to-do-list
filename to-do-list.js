e4b955a72a7e4105fdf93d615c8ddcd9a748438a
jyoshithaa_srinivasan <jyoshithaasrinivasan@gmail.com>
2024-04-23 17:16:02 +0530

    using eventlisteners instead of onclick attribute

--- to-do-list.js
+++ to-do-list.js
@@ -1,16 +1,23 @@
 const todoList=JSON.parse(localStorage.getItem('todoList'))||[
+
     {name:'make dinner',
      dueDate:'2022-12-22'
-    },
-    {
+},
+{
     name:'wash dishes',
     dueDate:'2022-11-04'
-    }
-    ];
-    renderTodoList();
-    function adTodo(){
+}
+];
+renderTodoList();
+
+document.querySelector('.js-add-todo-button').addEventListener('click',()=>{
+    adTodo();
+})
+
+function adTodo(){
     const inputNameElement=document.querySelector('.js-name-input');//class selector is parameter
     const inputDateElement=document.querySelector('.js-date-input');
+    
     const name=inputNameElement.value;
     const dueDate=inputDateElement.value;
     const inputObject={
@@ -19,49 +26,29 @@ const todoList=JSON.parse(localStorage.getItem('todoList'))||[
         //shorthand property
         //name,
         //dueDate
+    
     }
     todoList.push(inputObject);
     //localStorage.setItem('todoListArray',JSON.stringify(todoList));
+    
     console.log(todoList);
     inputNameElement.value='';
     inputDateElement.value='';
     //resets the text in the textbox on clicking add button
+
     renderTodoList();
     saveToStorage();
-    }
-    function renderTodoList(){
+   
+}
+
+function renderTodoList(){
      //let fetchArray=todoList;
     //if(localStorage.getItem('todoListArray')!=null){
      //fetchArray=JSON.parse(localStorage.getItem('todoListArray'));}
     //displaying
+    
     let todoListHTML='';
-    todoList.forEach((todoObject,index)=>{
-        
-        const name=todoObject.name;
-        //SHORTCUT
-        // const {name}=todoObject;
-        //const {name,dueDate}=todo
-        const dueDate=todoObject.dueDate;
-        //template strings as multiline strings
-        const html=`                 
-        <div> ${name}</div>    
-        <div>
-            ${dueDate}
-        </div>
-        <button onclick="
-            todoList.splice(${index},1);
-            renderTodoList();    
-            saveToStorage();
-        " class="delete-todo-button">DELETE</button>
-        `; //GENERATING HTML through javascript loop
-        //first div ll work as 1st column
-        //second div ll work as 2nd column
-        //button ll work as 3rd column in CSS GRID
-        todoListHTML+=html;
-        
-    }
-    );
-    /*//for(let i=0;i<fetchArray.length;i++){
+    //for(let i=0;i<fetchArray.length;i++){
     for(let i=0;i<todoList.length;i++){
         //const todoObject=fetchArray[i];
         const todoObject=todoList[i];
@@ -76,21 +63,33 @@ const todoList=JSON.parse(localStorage.getItem('todoList'))||[
         <div>
             ${dueDate}
         </div>
-        <button onclick="
-            todoList.splice(${i},1);
-            renderTodoList();    
-            saveToStorage();
-        " class="delete-todo-button">DELETE</button>
+
+        <button  class="delete-todo-button js-delete-todo-button">DELETE</button>
         `; //GENERATING HTML through javascript loop
         //first div ll work as 1st column
         //second div ll work as 2nd column
         //button ll work as 3rd column in CSS GRID
         todoListHTML+=html;
+        //here we are just generating the html, the button is still not on the page yet
         
-    }*/
+
+    }
+    //this code is for putting the button "on the page"-->after that only u can add eventlistenerr
     document.querySelector('.js-todo-list').innerHTML=todoListHTML;
-    }
-    function saveToStorage(){
+    document.querySelectorAll('.js-delete-todo-button')
+        .forEach((deleteButton,index)=>{
+            deleteButton.addEventListener('click',()=>{
+                todoList.splice(index,1);
+                renderTodoList();    
+                saveToStorage();
+
+            });
+
+        });
+
+
+}
+
+function saveToStorage(){
     localStorage.setItem('todoList',JSON.stringify(todoList));
-    }
-    
+}
